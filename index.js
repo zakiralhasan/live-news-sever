b9661a934db884d756f6a54832a9fc1eb023a6e0
zakiralhasan <zakiralhasan.bd@gmail.com>
2022-10-20 16:49:39 +0600

    news and category API created

--- index.js
+++ index.js
@@ -5,13 +5,40 @@ const port = 5000;
 app.use(cors()); //used for cors policy
 
 const categories = require("./data/categories.json");
+const allNews = require("./data/news.json");
 
 app.get("/", (req, res) => {
   res.send("Server is running and open");
 });
 
+//show category list
 app.get("/categories", (req, res) => {
   res.send(categories);
+});
+
+//select category news
+app.get("/category/:categoryID", (req, res) => {
+  const id = req.params.categoryID;
+  if (id === "08") {
+    res.send(allNews);
+  } else {
+    const selectedCategory = allNews.filter(
+      (category) => category.category_id === id
+    );
+    res.send(selectedCategory);
+  }
+});
+
+//show all news
+app.get("/news", (req, res) => {
+  res.send(allNews);
+});
+
+//select news
+app.get("/news/:newsID", (req, res) => {
+  const id = req.params.newsID;
+  const selectedNews = allNews.find((news) => news._id === id);
+  res.send(selectedNews);
 });
 
 app.listen(port, () => {
